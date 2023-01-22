51097b93f3024eabfa5819fc4390944b1f682880
aldiramadani00 <alditipex123@gmail.com>
2023-01-23 01:41:32 +0700

    Tugas
    Tugas

---
+++ hellovue/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+  data() {
+    return {
+      datasensor: "60",
+      datasensor2: "80",
+    };
+  },
+  created(){
+    this.getData()
+  },
+  methods:{
+    async getData(){
+      const data = await fetch("http://localhost:80/sensor1")
+      const datajson = await data.json()
+      console.log(datajson)
+      //this.datasensor = 100
+    },
+  },
+};
+
+</script>
+
+<template>
+<div>
+  hello {{ datasensor }}
+  <button @click="getData">Ambil Data</button>
+</div>
+</template>
---
+++ hellovue/src/main.js
@@ -0,0 +1,6 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import './input.css'
+
+createApp(App).mount('#app')
